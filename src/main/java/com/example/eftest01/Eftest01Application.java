080f93d547f29cf3f315094d35c16fc5c47bf8d6
yefei <yeifei@git.com>
2018-02-23 11:52:51 +0800

    初次提交

---
+++ src/main/java/com/example/eftest01/Eftest01Application.java
@@ -0,0 +1,12 @@
+package com.example.eftest01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Eftest01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Eftest01Application.class, args);
+	}
+}
